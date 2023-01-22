725cca30e03b2c783a91f28a6f4ec4f3d7063c39
azad <azad.shahbazov93@gmail.com>
2023-01-22 23:17:53 +0400

    changes

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const data = [
+    {
+        id: 1,
+        body: `### This just in!`
+
+    },
+    { id: 2, body: `a` },
+    { id: 3, body: `b` },
+    { id: 4, body: `c` },
+    { id: 5, body: `d` },
+    { id: 6, body: `e` },
+]
